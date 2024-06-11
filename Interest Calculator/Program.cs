3cc0c330d9d3235ed04d4467dc0c53425e4dd9a6
Jesse Bullard <jessebullard1996@gmail.com>
2024-06-11 13:56:03 -0600

    re-created interest rate calculator using methods

--- Interest Calculator/Program.cs
+++ Interest Calculator/Program.cs
@@ -1,34 +1,102 @@
-﻿namespace Interest_Calculator
+﻿// reference site for math https://www.investor.gov/financial-tools-calculators/calculators/compound-interest-calculator
+
+namespace Interest_Calculator
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // this is where you enter your principal for the loan
-            Console.WriteLine("Please enter your total loan amount: ");
-            double principal = Convert.ToDouble(Console.ReadLine());
+            // storing of variables
+            double principal = 0.00;
+            double rate = 0.00;
+            int timeInMonths = 0;
+            double timeInYears = 0;
+            int compound = 0;
+            double loanTotal = 0.00;
+            double interestTotal = 0.00;
+            string input = "";
 
-            // this is where you enter your loan interest rate and it is converted from % to double
-            Console.WriteLine("Please enter your interest rate: ");
-            double rate = Convert.ToDouble(Console.ReadLine()) / 100;
+            // calling of methods to execute code
+            GetLoanAmount(out principal);
+            GetInterestRate(out rate);
+            GetLoanLength(out timeInMonths, out timeInYears);
+            GetLoanCompoundAmount(out compound);
 
-            // this is where you enter the length of your total loan
-            Console.WriteLine("Now please enter your loan term length in months: ");
-            double time = Convert.ToDouble(Console.ReadLine());
-            double years = time / 12;
+            // getting final totals 
+            loanTotal = Math.Round(GetLoanTotal(principal, rate, compound, timeInYears), 2);
+            interestTotal = Math.Round(loanTotal - principal, 2);
 
-            // this is where you enter how often your interest rate is compounded
-            Console.WriteLine("How many times will this interest compound per year?: ");
-            double compound = Convert.ToInt32(Console.ReadLine());
+            // formats final outputs to be more readable by user
+            string formattedLoanTotal = loanTotal.ToString("0,000.00");
+            string formattedInterestTotal = interestTotal.ToString("0,000.00");
 
-            // this calculates the total of your loan payment
-            double totalInterest = principal * Math.Pow(1 + rate / compound, compound * years);
+            // calling last method to display formated methods to the user
+            ShowAllLoanInformation(formattedLoanTotal, formattedInterestTotal);
 
-            // rounds out the code to having only 2 decimal points and shows total amount owed & total interest paid     
-            double totalAmount = Math.Round(totalInterest, 2);
-            double totalPaidInterest = Math.Round(totalAmount - principal, 2);
-            Console.WriteLine($"Total Payment: {totalAmount}");
-            Console.WriteLine($"Your total interest paid: {totalPaidInterest}");
+        }
+        // method to handle all user input
+        static double GetUserInput(string message, bool isPercentage = false, bool isDollar = false)
+        {
+            double input;
+            while (true)
+            {
+                Console.Write(message);
+                string userInput = Console.ReadLine();
+                if (isDollar)
+                {
+                    userInput = userInput.TrimStart('$');
+                }
+                if (isPercentage)
+                {
+                    userInput = userInput.TrimEnd('%');
+                }
+                if (!double.TryParse(userInput, out input))
+                {
+                    Console.WriteLine("Please enter a number...");
+                }
+                else
+                {
+                    return input;
+                }
+            }
+        }
+
+        //greets user and gets initial loan amount
+        static void GetLoanAmount(out double principal)
+        {
+            Console.WriteLine("COMPOUND INTEREST CALCULATOR");
+            principal = GetUserInput("Please enter your total loan amount to begin: ", false, true);
+        }
+
+        //gets interest rate for users loan
+        static void GetInterestRate(out double rate)
+        {
+            rate = GetUserInput("Please enter your loans interest rate: ", true, false);
+            rate = rate / 100;
+        }
+
+        //gets loan length and converts it to years
+        static void GetLoanLength(out int timeInMonths, out double timeInYears)
+        {
+            timeInMonths = Convert.ToInt32(GetUserInput("Please enter the length for your loan in months: "));
+            timeInYears = timeInMonths / 12;
+        }
+        //gets the amount of times the interest will compound per year
+        static void GetLoanCompoundAmount(out int compound)
+        {
+            compound = Convert.ToInt32(GetUserInput("How many times will the interest rate compound per year?: "));
+        }
+
+        //calculates loans total paid after terms are completed
+        static double GetLoanTotal(double principal, double rate, int compound, double timeInYears)
+        {
+            return principal * Math.Pow((1 + (rate / compound)), (compound * timeInYears));
+        }
+
+        static void ShowAllLoanInformation(string loanTotal, string totalInterest)
+        {
+            Console.WriteLine($"\nYour total loan amount is ${loanTotal}");
+            Console.WriteLine($"Your total interest paid is ${totalInterest}");
         }
     }
 }
